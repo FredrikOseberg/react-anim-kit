963c1d3f7852bccab76311b2d53da42ee72ca59d
Fredrik Strand Oseberg <fredrik.strand.oseberg@avinor.no>
2020-04-07 13:11:16 +0200

    Change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,25 @@
-var path = require('path');
+var path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'index.js',
-    libraryTarget: 'commonjs2'
+    path: path.resolve(__dirname, "build"),
+    filename: "index.js",
+    libraryTarget: "commonjs2",
   },
   module: {
     rules: [
       {
         test: /\.js|jsx$/,
-        exclude: /(node_modules|bower_components|build)/,
+        include: path.resolve(__dirname, "src"),
+        exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
-        }
-      }
-    ]
+          loader: "babel-loader",
+        },
+      },
+    ],
   },
   externals: {
-    react: 'commonjs react'
-  }
+    react: "commonjs react",
+  },
 };
